c44a1361543222e8ecb522f26f0efce3b793a9c4
Safa2027 <safaalouane98@gmail.com>
2024-12-24 13:57:40 +0100

    integrate login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,85 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './login.component.html', 
+  styleUrls: ['./login.component.css'] ,
+  imports: [ FormsModule, CommonModule,HttpClientModule],
 })
-export class LoginComponent {
+export class LoginPageComponent {
+  email: string = '';
+  password: string = '';
+  forgotEmail: string = ''; 
+  Islogin : boolean = false ;
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const loginData = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.http.post('http://127.0.0.1:8000/login/', loginData).subscribe(
+      (response: any) => {
+        if (response.message === 'Login successful') {
+          alert('Connexion réussie');
+          console.log(response);
+          localStorage.setItem('user_id', response.user_id);
+          localStorage.setItem('role', response.role);
+
+        } else {
+          alert('E-mail ou mot de passe invalide');
+        }
+      },
+      (error) => {
+        console.error('Error:', error);
+        alert('Une erreur est survenue lors de la connexion');
+      }
+    );
+  }
+
+
+
+  isModalOpen = false; 
+
+  
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  // Forgot password 
+  resetPassword() {
+    if (!this.forgotEmail) {
+      alert('Veuillez entrer votre email');
+      return;
+    }
+
+    const resetData = { email: this.forgotEmail };
+
+    this.http.post('http://127.0.0.1:8000/forgot-password/', resetData).subscribe(
+      (response: any) => {
+        if (response.message === 'Password reset email sent successfully.') {
+          alert('Un email de réinitialisation a été envoyé.');
+          this.closeModal();  
+        } else {
+          alert('Erreur lors de l\'envoi de l\'email de réinitialisation');
+        }
+      },
+      (error) => {
+        console.error('Error:', error);
+        alert('Une erreur est survenue lors de la réinitialisation du mot de passe');
+      }
+    );
+  }
 }
+
+
